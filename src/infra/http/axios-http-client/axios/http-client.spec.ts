2e76ff11b1e2fb8c7717f4e73cdb4bff5a566b8f
bebossi <bernardobossi77@gmail.com>
2023-12-29 11:26:40 +0000

    refactor: move post request creation to a factory method

--- src/infra/http/axios-http-client/axios/http-client.spec.ts
+++ src/infra/http/axios-http-client/axios/http-client.spec.ts
@@ -1,18 +1,30 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { faker } from '@faker-js/faker';
 import axios from 'axios';
 import { AxiosHttpClient } from './axios-http-client';
+import { HttpPostParams } from '@/data/protocols/http';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 const makeSut = (): AxiosHttpClient => new AxiosHttpClient();
 
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.getMetadata(),
+});
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct URL and verb', async () => {
     const url = faker.internet.url();
     const sut = makeSut();
     await sut.post({ url });
-    // eslint-disable-next-line @typescript-eslint/unbound-method
     expect(mockedAxios.post).toHaveBeenCalledWith(url);
+  });
+
+  test('Should call axios with correct body', async () => {
+    const request = mockPostRequest();
+    const sut = makeSut();
+    await sut.post(request);
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url);
   });
 });
