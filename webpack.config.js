34a4ff6f9cc570004f15e2cee2682555fc1cf817
bebossi <bernardobossi77@gmail.com>
2023-12-30 16:30:19 +0000

    fix: change some configs to webpack display react in browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/main/index.tsx',
   output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    fileName: 'bundle.js',
+    path: path.resolve(__dirname, 'public/js'),
+    publicPath: '/public/js/',
+    filename: 'bundle.js',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
@@ -16,13 +17,29 @@ module.exports = {
     },
   },
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
+    static: {
+      directory: './public',
+    },
     historyApiFallback: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
   },
   externals: {
     react: 'React',
-    'react-dom': 'ReactDom',
+    'react-dom': 'ReactDOM',
   },
   plugins: [new CleanWebpackPlugin()],
 };
