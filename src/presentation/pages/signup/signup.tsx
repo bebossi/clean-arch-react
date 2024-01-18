9b33a58d3be7fe91de466c022341ba425b393ca4
bebossi <bernardobossi77@gmail.com>
2024-01-18 19:16:54 +0000

    feat: ensure Signup starts with correct initial state

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,14 +1,22 @@
 /* eslint-disable no-constant-condition */
-import React from 'react';
+import React, { useState } from 'react';
 import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components';
 import Context from '@/presentation/contexts/form/form-context';
-import { Link } from 'react-router-dom';
 
 const Signup: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    nameError: 'Required field',
+    emailError: 'Required field',
+    passwordError: 'Required field',
+    passwordConfirmationError: 'Required field',
+    mainError: '',
+  });
+
   return (
     <div className="flex flex-col h-screen justify-between bg-slate-100">
       <LoginHeader />
-      <Context.Provider value={{ state: {} }}>
+      <Context.Provider value={{ state }}>
         <form className="flex flex-col w-[400px] bg-white p-[40px] rounded-lg self-center shadow-md">
           <h2 className="text-rose-900 text-center text-xl font-bold ">Sign up</h2>
           <Input
@@ -35,15 +43,16 @@ const Signup: React.FC = () => {
             name="passwordConfirmation"
             placeholder="Confirm your password"
           />
-          <button className="bg-rose-500 mt-[32px] text-white rounded-lg text-base border-none leading-[60px">
+          <button
+            disabled
+            data-testid="submit"
+            className="bg-rose-500 mt-[32px] text-white rounded-lg text-base border-none leading-[60px"
+          >
             Sign up
           </button>
-          <Link
-            to="/login"
-            className="text-center text-rose-500 mt-[16px] cursor-pointer hover:underline"
-          >
+          <span className="text-center text-rose-500 mt-[16px] cursor-pointer hover:underline">
             <span data-testid="signup">Already have an account? Login page</span>
-          </Link>
+          </span>
           <FormStatus />
         </form>
       </Context.Provider>
