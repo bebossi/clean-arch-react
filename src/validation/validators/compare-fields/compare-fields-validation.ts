5ac1f4e8fc0daf58da1bf3ec6e1bb243d08ff3e0
bebossi <bernardobossi77@gmail.com>
2024-01-18 18:37:42 +0000

    feat: ensure CompareFieldsValidation returns falsy ifvalidation succeds

--- src/validation/validators/compare-fields/compare-fields-validation.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -2,8 +2,8 @@ import { InvalidFieldError } from '@/validation/errors';
 import { FieldValidation } from '@/validation/protocols/field-validation';
 
 export class CompareFieldsValidation implements FieldValidation {
-  constructor(readonly field: string) {}
+  constructor(readonly field: string, private readonly valueToCompare: string) {}
   validate(value: string): Error {
-    return new InvalidFieldError();
+    return value !== this.valueToCompare ? new InvalidFieldError() : null;
   }
 }
