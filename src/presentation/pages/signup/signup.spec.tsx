e74d545fade2fdef20ed48f7232679315929cf9d
bebossi <bernardobossi77@gmail.com>
2024-01-19 18:45:22 +0000

    test: ensure Signup enables submit button if form is valid

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -86,4 +86,14 @@ describe('Signup Component', () => {
     Helper.populateField(sut, 'passwordConfirmation');
     Helper.testStatusForField(sut, 'passwordConfirmation');
   });
+
+  test('Should enable submit button is form is valid', () => {
+    const { sut } = makeSut();
+    Helper.populateField(sut, 'name');
+    Helper.populateField(sut, 'email');
+    Helper.populateField(sut, 'password');
+    Helper.populateField(sut, 'passwordConfirmation');
+
+    Helper.testButtonIsDisabled(sut, 'submit', false);
+  });
 });
