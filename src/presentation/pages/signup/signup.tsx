b09cb7f72ca2c1a06c267750861b32d4dd9ad432
bebossi <bernardobossi77@gmail.com>
2024-01-19 18:38:04 +0000

    feat: ensure Signup change passwordConfirmation status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -13,6 +13,7 @@ const Signup: React.FC<Props> = ({ validation }: Props) => {
     name: '',
     email: '',
     password: '',
+    passwordConfirmation: '',
     nameError: '',
     emailError: '',
     passwordError: 'Required field',
@@ -26,8 +27,12 @@ const Signup: React.FC<Props> = ({ validation }: Props) => {
       nameError: validation.validate('name', state.name),
       emailError: validation.validate('email', state.email),
       passwordError: validation.validate('password', state.password),
+      passwordConfirmationError: validation.validate(
+        'passwordConfirmation',
+        state.passwordConfirmation
+      ),
     });
-  }, [state.name, state.email, state.password]);
+  }, [state.name, state.email, state.password, state.passwordConfirmation]);
 
   return (
     <div className="flex flex-col h-screen justify-between bg-slate-100">
