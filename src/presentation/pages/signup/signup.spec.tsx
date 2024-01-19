ba35b9ca20985c33a3600e6d4fbc9dd0e0da8c3f
bebossi <bernardobossi77@gmail.com>
2024-01-19 20:28:52 +0000

    test: ensure Signup prevent submit if form is invalid

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -148,4 +148,12 @@ describe('Signup Component', () => {
     await simulateValidSubmit(sut);
     expect(addAccountSpy.callsCount).toBe(1);
   });
+
+  test('Should not call authentication if form is invalid', async () => {
+    const validationError = faker.word.words();
+    const { sut, addAccountSpy } = makeSut({ validationError });
+    await simulateValidSubmit(sut);
+
+    expect(addAccountSpy.callsCount).toBe(0);
+  });
 });
