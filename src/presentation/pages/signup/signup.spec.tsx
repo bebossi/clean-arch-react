3bdadb5de2ec4a05a53afeaa6d23656a6ec0c076
bebossi <bernardobossi77@gmail.com>
2024-01-19 18:52:14 +0000

    test: ensure Signup shows spinner on submit

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import Signup from './signup';
 import { Helper, ValidationStub } from '@/presentation/test';
-import { RenderResult, cleanup, render } from '@testing-library/react';
+import {
+  RenderResult,
+  cleanup,
+  fireEvent,
+  render,
+  waitFor,
+} from '@testing-library/react';
 import { faker } from '@faker-js/faker';
 
 type SutTypes = {
@@ -19,6 +25,27 @@ const makeSut = (params?: SutParams): SutTypes => {
   return {
     sut,
   };
+};
+
+const testElementExists = (sut: RenderResult, fieldName: string): void => {
+  const el = sut.getByTestId(fieldName);
+  expect(el).toBeTruthy();
+};
+
+const simulateValidSubmit = async (
+  sut: RenderResult,
+  name = faker.person.firstName(),
+  email = faker.internet.email(),
+  password = faker.internet.password()
+): Promise<void> => {
+  Helper.populateField(sut, 'name', name);
+  Helper.populateField(sut, 'email', email);
+  Helper.populateField(sut, 'password', password);
+  Helper.populateField(sut, 'passwordConfirmation', password);
+
+  const form = sut.getByTestId('form');
+  fireEvent.submit(form);
+  await waitFor(() => form);
 };
 
 describe('Signup Component', () => {
@@ -95,5 +122,11 @@ describe('Signup Component', () => {
     Helper.populateField(sut, 'passwordConfirmation');
 
     Helper.testButtonIsDisabled(sut, 'submit', false);
+  });
+
+  test('Should show spinner on submit', async () => {
+    const { sut } = makeSut();
+    await simulateValidSubmit(sut);
+    testElementExists(sut, 'spinner');
   });
 });
