de241a46ebb8c133efe6c16a11a2ca62a490ad31
bebossi <bernardobossi77@gmail.com>
2024-01-22 17:27:05 +0000

    test: add SignupValidation factory

---
+++ src/main/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,20 @@
+import { ValidationComposite } from '@/validation/validators';
+import { ValidationBuilder } from '@/validation/validators/builder/validation-builder';
+import { makeSignupValidation } from './signup-validation-factory';
+
+describe('SignupValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignupValidation();
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...ValidationBuilder.field('name').required().minLength(3).build(),
+        ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().minLength(5).build(),
+        ...ValidationBuilder.field('passwordConfirmation')
+          .required()
+          .minLength(5)
+          .build(),
+      ])
+    );
+  });
+});
