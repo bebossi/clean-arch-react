79939d22f8525d002e97a79749f6e5749053d603
bebossi <bernardobossi77@gmail.com>
2024-01-22 17:26:55 +0000

    feat: add SignupValidation factory

---
+++ src/main/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,11 @@
+import { ValidationComposite } from '@/validation/validators';
+import { ValidationBuilder } from '@/validation/validators/builder/validation-builder';
+
+export const makeSignupValidation = (): ValidationComposite => {
+  return ValidationComposite.build([
+    ...ValidationBuilder.field('name').required().minLength(3).build(),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().minLength(5).build(),
+    ...ValidationBuilder.field('passwordConfirmation').required().minLength(5).build(),
+  ]);
+};
