7b7b1433f50f08615a3c44ebb5b51df2d1f09334
bebossi <bernardobossi77@gmail.com>
2024-01-24 14:14:37 +0000

    fix: update color on changing state correctly

--- src/presentation/components/submit-button/submit-button.tsx
+++ src/presentation/components/submit-button/submit-button.tsx
@@ -1,23 +1,26 @@
-import React, { useContext } from 'react';
-import Context from '@/presentation/contexts/form/form-context';
+import React, { useContext } from 'react'
+import Context from '@/presentation/contexts/form/form-context'
 
 type Props = {
-  text: string;
-};
+  text: string
+}
 const SubmitButton: React.FC<Props> = ({ text }: Props) => {
-  const { state } = useContext(Context);
+  const { state } = useContext(Context)
+  console.log(state.isFormInvalid)
   return (
     <button
       disabled={state.isFormInvalid}
       type="submit"
       data-testid="submit"
-      className={`bg-rose-500 mt-[32px] text-white rounded-lg text-base border-none leading-[60px] ${
-        state.isFormInvalid ? 'bg-gray-400 text-gray-700 hover:opacity-100' : ''
+      className={` mt-[32px] text-white rounded-lg text-base border-none leading-[60px] ${
+        state.isFormInvalid
+          ? 'bg-gray-400 text-gray-700 hover:opacity-100'
+          : 'bg-rose-500'
       }`}
     >
       {text}
     </button>
-  );
-};
+  )
+}
 
-export default SubmitButton;
+export default SubmitButton
