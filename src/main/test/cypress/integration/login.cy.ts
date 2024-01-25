8aa2f75cf9e4a881c47d1af3c3e23405e7cf2d48
bebossi <bernardobossi77@gmail.com>
2024-01-25 17:52:32 +0000

    test: ensure Login present error if invalid credentials are provided

--- src/main/test/cypress/integration/login.cy.ts
+++ src/main/test/cypress/integration/login.cy.ts
@@ -21,7 +21,7 @@ describe('Login', () => {
       .should('have.attr', 'title', 'Invalid field')
       .should('contain.text', '🔴')
     cy.getByTestId('password').type(
-      faker.string.alphanumeric({ length: { min: 0, max: 3 } })
+      faker.string.alphanumeric({ length: { min: 1, max: 3 } })
     )
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Invalid field')
@@ -44,5 +44,25 @@ describe('Login', () => {
       .should('contain.text', '🟢')
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').type(faker.internet.email())
+
+    cy.getByTestId('password').type(
+      faker.string.alphanumeric({ length: { min: 5, max: 12 } })
+    )
+
+    cy.getByTestId('submit').click()
+
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('exist')
   })
 })
