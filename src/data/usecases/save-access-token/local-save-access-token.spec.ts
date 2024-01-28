166f175ce413f9630f432054947ac5269adbff0b
bebossi <bernardobossi77@gmail.com>
2024-01-28 15:39:27 +0000

    test: ensure LocalSaveAccessToken throw if no accessToken is provided

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -1,34 +1,41 @@
-import { faker } from '@faker-js/faker';
-import { LocalSaveAccessToken } from './local-save-access-token';
-import { SetStorageMock } from '@/data/test';
+import { faker } from '@faker-js/faker'
+import { LocalSaveAccessToken } from './local-save-access-token'
+import { SetStorageMock } from '@/data/test'
+import { UnexpectedError } from '@/domain/errors'
 
 type SutTypes = {
-  sut: LocalSaveAccessToken;
-  setStorageMock: SetStorageMock;
-};
+  sut: LocalSaveAccessToken
+  setStorageMock: SetStorageMock
+}
 
 const makeSut = (): SutTypes => {
-  const setStorageMock = new SetStorageMock();
-  const sut = new LocalSaveAccessToken(setStorageMock);
+  const setStorageMock = new SetStorageMock()
+  const sut = new LocalSaveAccessToken(setStorageMock)
   return {
     sut,
     setStorageMock,
-  };
-};
+  }
+}
 
 describe('LocalSaveAccessToekn', () => {
   test('Should call SetStorage with correct values', async () => {
-    const { sut, setStorageMock } = makeSut();
-    const accesToken = faker.string.uuid();
-    await sut.save(accesToken);
-    expect(setStorageMock.key).toBe('accessToken');
-    expect(setStorageMock.value).toBe(accesToken);
-  });
+    const { sut, setStorageMock } = makeSut()
+    const accesToken = faker.string.uuid()
+    await sut.save(accesToken)
+    expect(setStorageMock.key).toBe('accessToken')
+    expect(setStorageMock.value).toBe(accesToken)
+  })
 
   test('Should trhow is SetStorage throws', async () => {
-    const { sut, setStorageMock } = makeSut();
-    jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error());
-    const promise = sut.save(faker.string.uuid());
-    await expect(promise).rejects.toThrow(new Error());
-  });
-});
+    const { sut, setStorageMock } = makeSut()
+    jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error())
+    const promise = sut.save(faker.string.uuid())
+    await expect(promise).rejects.toThrow(new Error())
+  })
+
+  test('Should trhow if accessToken is falsy', async () => {
+    const { sut } = makeSut()
+    const promise = sut.save(undefined)
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+})
