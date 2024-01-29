7e38ab4d6593c83c4913326dc055244ae75dac88
bebossi <bernardobossi77@gmail.com>
2024-01-29 21:41:33 +0000

    test: ensure Signup present valid state if form is valid

--- src/main/test/cypress/integration/signup.cy.ts
+++ src/main/test/cypress/integration/signup.cy.ts
@@ -33,4 +33,19 @@ describe('Signup', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
+
+  it('Should present valid state if form is valid', () => {
+    cy.getByTestId('name').type(faker.person.fullName())
+    FormHelper.testInputStatus('name')
+    cy.getByTestId('email').type(faker.internet.email())
+    FormHelper.testInputStatus('email')
+
+    const password = faker.string.alphanumeric({ length: { min: 5, max: 12 } })
+    cy.getByTestId('password').type(password)
+    FormHelper.testInputStatus('password')
+    cy.getByTestId('passwordConfirmation').type(password)
+    FormHelper.testInputStatus('passwordConfirmation')
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
 })
