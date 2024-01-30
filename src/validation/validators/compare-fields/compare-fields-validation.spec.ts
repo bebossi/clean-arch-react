a72df838c16ba36db4542153f47277d0e2167705
bebossi <bernardobossi77@gmail.com>
2024-01-30 18:30:33 +0000

    test: testing travis ci error

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -1,31 +1,31 @@
-import { InvalidFieldError } from '@/validation/errors';
-import { faker } from '@faker-js/faker';
-import { CompareFieldsValidation } from './compare-fields-validation';
+import { InvalidFieldError } from '@/validation/errors'
+import { faker } from '@faker-js/faker'
+import { CompareFieldsValidation } from './compare-fields-validation'
 
 const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation =>
-  new CompareFieldsValidation(field, fieldToCompare);
+  new CompareFieldsValidation(field, fieldToCompare)
 
 describe('CompareFieldsValidation', () => {
   test('Should return error if compare is invalid', () => {
-    const field = faker.database.column();
-    const fieldToCompare = faker.database.column();
-    const sut = makeSut(field, fieldToCompare);
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
+    const sut = makeSut(field, fieldToCompare)
     const error = sut.validate({
       [field]: faker.word.verb(),
       [fieldToCompare]: faker.word.verb(),
-    });
-    expect(error).toEqual(new InvalidFieldError());
-  });
+    })
+    expect(error).toEqual(new InvalidFieldError())
+  })
 
   test('Should return falsy if compare is valid', () => {
-    const field = faker.database.column();
-    const fieldToCompare = faker.database.column();
-    const value = faker.word.verb();
-    const sut = makeSut(field, fieldToCompare);
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
+    const value = faker.word.verb()
+    const sut = makeSut(field, fieldToCompare)
     const error = sut.validate({
       [field]: value,
       [fieldToCompare]: value,
-    });
-    expect(error).toBeFalsy();
-  });
-});
+    })
+    expect(error).toBeFalsy()
+  })
+})
