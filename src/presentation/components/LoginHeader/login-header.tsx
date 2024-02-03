9079e255a141a47bb0f1024f526668418297cc30
bebossi <bernardobossi77@gmail.com>
2024-02-03 10:37:03 +0000

    refactor: update margin on header/logo

--- src/presentation/components/LoginHeader/login-header.tsx
+++ src/presentation/components/LoginHeader/login-header.tsx
@@ -1,15 +1,15 @@
-import React, { memo } from 'react';
-import Logo from '../Logo/logo';
+import React, { memo } from 'react'
+import Logo from '../Logo/logo'
 
 const LoginHeader: React.FC = () => {
   return (
-    <header className="bg-rose-700 flex flex-col border-t-[40px] border-t-rose-900 items-center">
-      <Logo />
-      <h1 className="text-white mt-[16px] mb-[40px] ">
-        4Dev - Poll for developers
-      </h1>
+    <header className="bg-rose-700 flex flex-col border-t-[40px] border-t-rose-900 items-center ">
+      <div className="mt-[40px]">
+        <Logo />
+        <h1 className="text-white mt-[16px] mb-[40px] ">4Dev - Poll for developers</h1>
+      </div>
     </header>
-  );
-};
+  )
+}
 
-export default memo(LoginHeader);
+export default memo(LoginHeader)
