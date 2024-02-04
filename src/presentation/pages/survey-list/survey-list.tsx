0ff5ee48d9eaaa63234483e9889b374b49b21578
bebossi <bernardobossi77@gmail.com>
2024-02-04 12:58:43 +0000

    feat: add skeleton shimmer effect

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -27,40 +27,11 @@ const SurveyList: React.FC = () => {
               See result
             </footer>
           </li>
-          <li className="h-[250px]  bg-white flex flex-col justify-between rounded-lg  sm:basis-[48%] mb-[24px] shadow-md  ">
-            {/* survey content */}
-            <div className="flex flex-row relative  rounded-t-md justify-between flex-grow bg-no-repeat bg-gradient-to-r from-rose-500 to-rose-500 linear-gradient ">
-              <Icon iconName={IconName.thumbDown} />
-              <time className="flex flex-col bg-rose-500 text-white rounded-md ml-[24px] self-center justify-center items-center w-[60px] h-[100px] flex-shrink-0 ">
-                <span className="text-[40px] font-bold ">22</span>
-                <span className="lowercase  ">03</span>
-                <span className="text-[16px]">2024</span>
-              </time>
-              <p className="text-[18px] m-[24px] self-center ">
-                What are your favorite web framework
-              </p>
-            </div>
-            <footer className="bg-rose-500 text-white leading-[40px] lowercase text-center cursor-pointer hover:bg-rose-950 rounded-b-md ">
-              See result
-            </footer>
-          </li>
-          <li className="h-[250px]  bg-white flex flex-col justify-between rounded-lg  sm:basis-[48%] mb-[24px] shadow-md  ">
-            {/* survey content */}
-            <div className="flex flex-row relative  rounded-t-md justify-between flex-grow bg-no-repeat bg-gradient-to-r from-rose-500 to-rose-500 linear-gradient ">
-              <Icon iconName={IconName.thumbDown} />
-              <time className="flex flex-col bg-rose-500 text-white rounded-md ml-[24px] self-center justify-center items-center w-[60px] h-[100px] flex-shrink-0 ">
-                <span className="text-[40px] font-bold ">22</span>
-                <span className="lowercase  ">03</span>
-                <span className="text-[16px]">2024</span>
-              </time>
-              <p className="text-[18px] m-[24px] self-center ">
-                What are your favorite web framework
-              </p>
-            </div>
-            <footer className="bg-rose-500 text-white leading-[40px] lowercase text-center cursor-pointer hover:bg-rose-950 rounded-b-md ">
-              See result
-            </footer>
-          </li>
+
+          <li className="h-[250px] bg-white flex flex-col justify-between rounded-lg  sm:basis-[48%] mb-[24px] shadow-md skeleton"></li>
+          <li className="h-[250px] bg-white flex flex-col justify-between rounded-lg  sm:basis-[48%] mb-[24px] shadow-md skeleton"></li>
+          <li className="h-[250px] bg-white flex flex-col justify-between rounded-lg  sm:basis-[48%] mb-[24px] shadow-md skeleton"></li>
+          <li className="h-[250px] bg-white flex flex-col justify-between rounded-lg  sm:basis-[48%] mb-[24px] shadow-md skeleton"></li>
         </ul>
       </div>
       <Footer />
