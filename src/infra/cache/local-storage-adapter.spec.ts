b1f13b62d1ca35700cffb6c1cfff2ce5fa5ca565
bebossi <bernardobossi77@gmail.com>
2024-02-04 14:03:40 +0000

    refactor: update interaction with local storage to dont be async

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/unbound-method */
-import { faker } from '@faker-js/faker';
-import 'jest-localstorage-mock';
-import { LocalStorageAdapter } from './local-storage-adapter';
+import { faker } from '@faker-js/faker'
+import 'jest-localstorage-mock'
+import { LocalStorageAdapter } from './local-storage-adapter'
 
-const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter();
+const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
 describe('LocalStorageAdapter', () => {
   beforeEach(() => {
-    localStorage.clear();
-  });
+    localStorage.clear()
+  })
   test('Should call localStorage with correct value ', async () => {
-    const sut = makeSut();
-    const key = faker.database.column();
-    const value = faker.string.alpha();
-    await sut.set(key, value);
-    expect(localStorage.setItem).toHaveBeenCalledWith(key, value);
-  });
-});
+    const sut = makeSut()
+    const key = faker.database.column()
+    const value = faker.string.alpha()
+    sut.set(key, value)
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
+  })
+})
--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -1,8 +1,8 @@
-import { SetStorage } from '@/data/protocols/cache/set-storage';
+import { SetStorage } from '@/data/protocols/cache/set-storage'
 
 export class LocalStorageAdapter implements SetStorage {
-  async set(key: string, value: any): Promise<void> {
+  set(key: string, value: any): void {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    localStorage.setItem(key, value);
+    localStorage.setItem(key, value)
   }
 }
