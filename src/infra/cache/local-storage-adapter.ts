3bb75851230630fba7b98c95d9752d5fb5c26bb3
bebossi <bernardobossi77@gmail.com>
2024-02-04 15:50:20 +0000

    feat: ensure LocalStorageAdapter calls localstorage.getItem with correct value

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -1,8 +1,12 @@
-import { SetStorage } from '@/data/protocols/cache/set-storage'
+import { GetStorage, SetStorage } from '@/data/protocols/cache'
 
-export class LocalStorageAdapter implements SetStorage {
+export class LocalStorageAdapter implements SetStorage, GetStorage {
   set(key: string, value: object): void {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  get(key: string): any {
+    return JSON.parse(localStorage.getItem(key))
   }
 }
