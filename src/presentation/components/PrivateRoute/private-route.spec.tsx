753e59ec5b760c1f096d9a5444f399897f99fd78
bebossi <bernardobossi77@gmail.com>
2024-02-04 16:50:49 +0000

    refactor: move Sut creation to a factory method

--- src/presentation/components/PrivateRoute/private-route.spec.tsx
+++ src/presentation/components/PrivateRoute/private-route.spec.tsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import PrivateRoute from './private-route'
-import { createMemoryHistory } from 'history'
+import { MemoryHistory, createMemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 import { ApiContext } from '@/presentation/contexts'
 import { AccountModel } from '@/domain/models'
 
+type SutTypes = {
+  history: MemoryHistory
+}
+const makeSut = (): SutTypes => {
+  const getCurrentAccount = (): AccountModel => ({
+    accessToken: '',
+    name: 'ugjkgiu',
+  })
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+  render(
+    <ApiContext.Provider value={{ getCurrentAccount }}>
+      <Router location={history.location} navigator={history}>
+        <PrivateRoute></PrivateRoute>
+      </Router>
+    </ApiContext.Provider>
+  )
+
+  return {
+    history,
+  }
+}
+
 describe('PrivateRoute', () => {
   test('Should redirect to /login if accessToken is empty', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] })
-    const getCurrentAccount = (): AccountModel => ({
-      accessToken: '',
-      name: 'ugjkgiu',
-    })
+    const { history } = makeSut()
 
-    render(
-      <ApiContext.Provider value={{ getCurrentAccount }}>
-        <Router location={history.location} navigator={history}>
-          <PrivateRoute></PrivateRoute>
-        </Router>
-      </ApiContext.Provider>
-    )
     expect(history.location.pathname).toBe('/login')
   })
 })
