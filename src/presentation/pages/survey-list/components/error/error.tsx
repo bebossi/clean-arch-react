423eea9948e73253de19ff43ad554cfa928ea46f
bebossi <bernardobossi77@gmail.com>
2024-02-09 16:23:01 +0000

    feat: chage LoadSurveyList error layout

--- src/presentation/pages/survey-list/components/error/error.tsx
+++ src/presentation/pages/survey-list/components/error/error.tsx
@@ -3,14 +3,14 @@ import { SurveyContext } from '@/presentation/pages/survey-list/components'
 
 const Error: React.FC = () => {
   const { state, setState } = useContext(SurveyContext)
-  // const reload = (): void => {
-  //   setState({ surveys: [], error: '', reload: !state.reload })
-  // }
 
   return (
-    <div>
-      <span data-testid="error">{state.error}</span>
+    <div className="flex flex-col bg-white p-[40px] items-center text-center rounded-md shadow-md ">
+      <span className="text-[20px] mb-[] " data-testid="error">
+        {state.error}
+      </span>
       <button
+        className=" mt-[32px] px-4 text-white rounded-lg text-base border-none leading-[50px] bg-rose-500 "
         data-testid="reload"
         onClick={() => setState({ surveys: [], error: '', reload: !state.reload })}
       >
