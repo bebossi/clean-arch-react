aa379ed8cdbf8142fa7044281b6ae031f24b75de
bebossi <bernardobossi77@gmail.com>
2024-02-11 16:19:23 +0000

    feat: ensure RemoteLoadSurveyList map remote date to domain data

--- src/domain/usecases/load-survey-list.ts
+++ src/domain/usecases/load-survey-list.ts
@@ -6,10 +6,6 @@ export namespace LoadSurveyList {
   export type Model = {
     id: string
     question: string
-    answers: Array<{
-      image?: string
-      answer: string
-    }>
     date: Date
     didAnswer: boolean
   }
