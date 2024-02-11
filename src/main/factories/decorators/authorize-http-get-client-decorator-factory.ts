4aeabd864cd1212bb649d0b41996cfe5d39c6b2b
bebossi <bernardobossi77@gmail.com>
2024-02-11 15:58:08 +0000

    feat: add AuthorizeHttpGetClientDecorator factory

---
+++ src/main/factories/decorators/authorize-http-get-client-decorator-factory.ts
@@ -0,0 +1,11 @@
+import { makeLocalStorageAdapter } from '@/main/factories/cache/local-storage-adapter-factory'
+import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory'
+import { AuthorizeHttpGetClientDecorator } from '@/main/decorators'
+import { HttpGetClient } from '@/data/protocols/http'
+
+export const makeAuthorizeHttpGetClientDecorator = (): HttpGetClient => {
+  return new AuthorizeHttpGetClientDecorator(
+    makeLocalStorageAdapter(),
+    makeAxiosHttpClient()
+  )
+}
