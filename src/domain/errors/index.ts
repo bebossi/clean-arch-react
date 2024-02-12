986657fd0b38f52c9689b7f179551c42c51b1e0e
bebossi <bernardobossi77@gmail.com>
2024-02-12 17:10:56 +0000

    feat: ensure RemoteLoadSurveyList throws correct error

--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -1,3 +1,4 @@
-export * from './invalid-credentials-error';
-export * from './unexpectedError';
-export * from './email-in-use-error';
+export * from './invalid-credentials-error'
+export * from './unexpectedError'
+export * from './email-in-use-error'
+export * from './access-denied-error'
