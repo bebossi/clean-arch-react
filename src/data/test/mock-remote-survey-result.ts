ffa860a54c241d44f6afa5cd755c0fbc1a3d4417
bebossi <bernardobossi77@gmail.com>
2024-02-18 18:10:56 +0000

    test: ensure RemoteLoadSurveyResult returns surveyResultModel on 200

---
+++ src/data/test/mock-remote-survey-result.ts
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker'
+import { RemoteLoadSurveyResult } from '../usecases'
+
+export const mockRemoteSurveyResultModel = (): RemoteLoadSurveyResult.Model => ({
+  question: faker.word.words(),
+  date: faker.date.recent().toISOString(),
+  answers: [
+    {
+      image: faker.internet.url(),
+      answer: faker.word.words(),
+      count: faker.number.int(),
+      percent: faker.number.int({ max: 100 }),
+    },
+    {
+      answer: faker.word.words(),
+      count: faker.number.int(),
+      percent: faker.number.int({ max: 100 }),
+    },
+  ],
+})
