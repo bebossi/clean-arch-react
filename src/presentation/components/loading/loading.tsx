86340ccb056de7b433ae1ad645281984c6fb0d0b
bebossi <bernardobossi77@gmail.com>
2024-02-20 21:58:17 +0000

    feat: ensure SurveyResult present correct initial state

--- src/presentation/components/loading/loading.tsx
+++ src/presentation/components/loading/loading.tsx
@@ -3,7 +3,10 @@ import Spinner from '../Spinner/spinner'
 
 const Loading: React.FC = () => {
   return (
-    <div className="flex items-center justify-center bg-white bg-opacity-80 fixed top-0 left-0 right-0 bottom-0 cursor-wait ">
+    <div
+      data-testid="loading"
+      className="flex items-center justify-center bg-white bg-opacity-80 fixed top-0 left-0 right-0 bottom-0 cursor-wait "
+    >
       <div className="flex flex-col items-center justify-center bg-black bg-opacity-90 w-[300px] h-[150px] rounded-md text-white ">
         <span className="font-[16px]">Wait ...</span>
         <Spinner />
--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,49 +1,63 @@
-import { Calendar, Footer, Header, Loading } from '@/presentation/components'
-import React from 'react'
+import { LoadSurveyResult } from '@/domain/usecases'
+import { Calendar, Error, Footer, Header, Loading } from '@/presentation/components'
+import React, { useState } from 'react'
 
 const SurveyList: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    error: '',
+    surveyResult: null as LoadSurveyResult.Model,
+  })
   return (
     // SurveyResultWrap
     <div className="flex flex-col justify-between min-h-[100vh] bg-gray-200  ">
       <Header />
       {/* // Content wrap */}
-      <div className="flex flex-col self-center max-w-[800px] w-full flex-grow p-[40px] skeletonResult">
-        <hgroup className="flex flex-row items-center mb-4  ">
-          <Calendar date={new Date()} className="w-[80px] mr-4 " />
-          <h2 className="text-rose-950 text-[28px] font-bold">
-            Qual seu framework web fav?
-          </h2>
-        </hgroup>
-        {/* answers list */}
-        <ul className="list-none flex flex-col">
-          <li className="flex justify-between bg-white items-center p-[16px] rounded-md text-rose-950 mt-[16px] active:border-[2px] active:border-rose-950 ">
-            <img src="" className="w-[50px] h-[50px] mr-4" />
-            {/* //Styles answer */}
-            <span className="flex-grow mr-4 text-[20px]">React.js</span>
-            {/* //percent */}
-            <span className="text-[30px]">50%</span>
-          </li>
-          <li className="flex justify-between bg-white items-center p-[16px] rounded-md text-rose-950 mt-[16px] active:border-[2px] active:border-rose-950 ">
-            <img src="" className="w-[50px] h-[50px]" />
-            {/* //Styles answer */}
-            <span className="flex-grow mx-4 text-[20px]">React.js</span>
-            {/* //percent */}
+      <div
+        data-testid="survey-result"
+        className="flex flex-col self-center max-w-[800px] w-full flex-grow p-[40px] skeletonResult"
+      >
+        {state.surveyResult && (
+          <>
+            <hgroup className="flex flex-row items-center mb-4  ">
+              <Calendar date={new Date()} className="w-[80px] mr-4 " />
+              <h2 className="text-rose-950 text-[28px] font-bold">
+                Qual seu framework web fav?
+              </h2>
+            </hgroup>
+            {/* answers list */}
+            <ul className="list-none flex flex-col">
+              <li className="flex justify-between bg-white items-center p-[16px] rounded-md text-rose-950 mt-[16px] active:border-[2px] active:border-rose-950 ">
+                <img src="" className="w-[50px] h-[50px] mr-4" />
+                {/* //Styles answer */}
+                <span className="flex-grow mr-4 text-[20px]">React.js</span>
+                {/* //percent */}
+                <span className="text-[30px]">50%</span>
+              </li>
+              <li className="flex justify-between bg-white items-center p-[16px] rounded-md text-rose-950 mt-[16px] active:border-[2px] active:border-rose-950 ">
+                <img src="" className="w-[50px] h-[50px]" />
+                {/* //Styles answer */}
+                <span className="flex-grow mx-4 text-[20px]">React.js</span>
+                {/* //percent */}
 
-            <span className="text-[30px]">50%</span>
-          </li>{' '}
-          <li className="flex justify-between bg-white items-center p-[16px] rounded-md text-rose-950 mt-[16px] active:border-[2px] active:border-rose-950 ">
-            <img src="" className="w-[50px] h-[50px]" />
-            {/* //Styles answer */}
-            <span className="flex-grow mx-4 text-[20px]">React.js</span>
-            {/* //percent */}
-            <span className="text-[30px]">50%</span>
-          </li>
-        </ul>
-        <button className="bg-rose-500 leading-[50px] text-white rounded-md text-[20px] px-[16px] mt-[16px] outline-none ">
-          Voltar
-        </button>
+                <span className="text-[30px]">50%</span>
+              </li>{' '}
+              <li className="flex justify-between bg-white items-center p-[16px] rounded-md text-rose-950 mt-[16px] active:border-[2px] active:border-rose-950 ">
+                <img src="" className="w-[50px] h-[50px]" />
+                {/* //Styles answer */}
+                <span className="flex-grow mx-4 text-[20px]">React.js</span>
+                {/* //percent */}
+                <span className="text-[30px]">50%</span>
+              </li>
+            </ul>
+            <button className="bg-rose-500 leading-[50px] text-white rounded-md text-[20px] px-[16px] mt-[16px] outline-none ">
+              Voltar
+            </button>
+          </>
+        )}
 
-        {false && <Loading />}
+        {state.isLoading && <Loading />}
+        {state.error && <Error error={state.error} reload={() => {}} />}
       </div>
       <Footer />
     </div>
