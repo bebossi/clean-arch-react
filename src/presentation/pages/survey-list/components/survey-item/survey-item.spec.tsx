208bf63174da60d8ab46b54348d063fe3f16d359
bebossi <bernardobossi77@gmail.com>
2024-02-20 21:40:24 +0000

    fix: move calendar tests back to survey-list

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -11,6 +11,7 @@ describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
       didAnswer: true,
+      date: new Date('2023-01-10T00:00:00'),
     })
     makeSut(survey)
     expect(screen.getByTestId('icon')).toHaveProperty(
@@ -18,11 +19,15 @@ describe('SurveyItem Component', () => {
       'http://localhost/icon-thumb-up.png'
     )
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('10')
+    expect(screen.getByTestId('month')).toHaveTextContent('jan')
+    expect(screen.getByTestId('year')).toHaveTextContent('2023')
   })
 
   test('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
       didAnswer: false,
+      date: new Date('2024-05-03T00:00:00'),
     })
     makeSut(survey)
     expect(screen.getByTestId('icon')).toHaveProperty(
@@ -30,5 +35,8 @@ describe('SurveyItem Component', () => {
       'http://localhost/icon-thumb-down.png'
     )
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('03')
+    expect(screen.getByTestId('month')).toHaveTextContent('mai')
+    expect(screen.getByTestId('year')).toHaveTextContent('2024')
   })
 })
