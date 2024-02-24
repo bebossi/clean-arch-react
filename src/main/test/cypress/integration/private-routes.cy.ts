d5c2ed1902a6120fda6b7acd043e27582a5b85c7
bebossi <bernardobossi77@gmail.com>
2024-02-24 13:59:06 +0000

    test: ensure SurveyResult redirects to login if no token is found

--- src/main/test/cypress/integration/private-routes.cy.ts
+++ src/main/test/cypress/integration/private-routes.cy.ts
@@ -4,4 +4,9 @@ describe('PrivateRoutes', () => {
     cy.visit('/')
     Helper.testUrl('/login')
   })
+
+  it('Should logout if survey-result has no token', () => {
+    cy.visit('/surveys/any_id')
+    Helper.testUrl('/login')
+  })
 })
