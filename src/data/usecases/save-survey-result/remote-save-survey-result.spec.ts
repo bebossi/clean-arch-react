7585ce5394d26c2f091e58501005432d66987d3a
bebossi <bernardobossi77@gmail.com>
2024-02-28 14:55:46 +0000

    test: ensure RemoteSaveSurveyResult returns AccessDeniedError on 403

--- src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
@@ -3,6 +3,7 @@ import { RemoteSaveSurveyResult } from '@/data/usecases'
 import { HttpClientSpy, mockRemoteSurveyResultModel } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
 import { mockSaveSurveyResultParams } from '@/domain/test'
+import { AccessDeniedError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RemoteSaveSurveyResult
@@ -31,5 +32,14 @@ describe('RemoteSaveSurveyResult', () => {
     expect(httpClientSpy.url).toBe(url)
     expect(httpClientSpy.method).toBe('put')
     expect(httpClientSpy.body).toEqual(saveSurveyResultParams)
+  })
+
+  test('Should throw AccessDeniedError if HttClient returns 403', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden,
+    }
+    const promise = sut.save(mockSaveSurveyResultParams())
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })
